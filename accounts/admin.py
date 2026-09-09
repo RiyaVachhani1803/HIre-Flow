08474bad8320535cba1e7b880390f5b4921c8d09
Riya Vachhani <riya.vachhani0001@gmail.com>
2026-09-09 09:16:48 +0530

    Add complete HireFlow project

--- accounts/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Employee
-
-@admin.register(Employee)
-class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ['full_name', 'employee_id', 'role', 'department', 'position', 'status']
-    list_filter  = ['role', 'department', 'status']
-    search_fields = ['full_name', 'email', 'employee_id']
